62c71367e318e6e432042c053b26e06c5d3a2036
Omar <omarabdelaty101@gmail.com>
2026-03-02 01:36:29 +0200

    feat: Add Stock and Comment models with one-to-many relationship

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a3768496865dba4313e2b0d8470ec4d3bd47c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48aca4bb6b5c068d33bdfcdeb03cc142b07307ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
